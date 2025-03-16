104cb76102339bd1fa26e3f8bc6fddbbdba23a9b
Morumbi <zun0323@gmail.com>
2025-03-16 20:27:55 -0300

    feat : set Map

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,19 @@
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
