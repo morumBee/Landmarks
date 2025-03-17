f37b53f14df9d05d66465463bfb12a99a7df40c5
Morumbi <zun0323@gmail.com>
2025-03-17 12:36:22 -0300

    feat : create Model

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -1,8 +1,26 @@
-//
-//  Landmark.swift
-//  Landmarks
-//
-//  Created by Morumbi on 3/17/25.
-//
-
+import CoreLocation
 import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    var image: Image { Image(imageName) }
+    var locationCoordinates: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+
+    private var imageName: String
+    private var coordinates: Coordinates
+}
